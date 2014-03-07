803e23f873fcfb71649c6311b617094e1ec18612
Zbigniew Braniecki <gandalf@mozilla.com>
2014-03-06 16:00:36 -0800

    land parser tests

--- tests/lib/parser.js
+++ tests/lib/parser.js
@@ -1,18 +1,14 @@
 var should = require('should');
-var Parser = process.env.L20N_COV
-  ? require('../../build/cov/lib/l20n/parser').Parser
-  : require('../../lib/l20n/parser').Parser;
+
+var parse = process.env.L20N_COV
+  ? require('../../build/cov/lib/l20n/parser').parseProperties
+  : require('../../lib/l20n/parser').parseProperties;
 
 describe('Example', function() {
-  var parser;
-  beforeEach(function() {
-    parser = new Parser();
-  });
-
   it('string value', function() {
-    var ast = parser.parse("id = string");
-    should.not.exist(ast.body['id'].type);
-    ast.body['id'].value.content.should.equal('string');
+    var ast = parse("id = string");
+    should.not.exist(ast['id'].type);
+    ast['id'].should.equal('string');
   });
   it('basic errors', function() {
     var strings = [
@@ -25,28 +21,23 @@ describe('Example', function() {
     ];
 
     for (var i in strings) {
-      var ast = parser.parse(strings[i]);
-      Object.keys(ast.body).length.should.equal(0);
+      var ast = parse(strings[i]);
+      Object.keys(ast).length.should.equal(0);
     }
   });
   it('basic attributes', function() {
-    var ast = parser.parse("id.attr1 = foo");
-    ast.body['id'].attrs['attr1'].value.content.should.equal('foo');
+    var ast = parse("id.attr1 = foo");
+    ast['id']['attr1'].should.equal('foo');
   });
   it('plural macro', function() {
-    var ast = parser.parse("id = {[ plural(m) ]} \nid[one] = foo");
-    ast.body['id'].value.type.should.equal('Hash');
-    ast.body['id'].value.content[0].type.should.equal('HashItem');
-    ast.body['id'].value.content[0].value.content.should.equal('foo');
-    ast.body['id'].index.length.should.equal(1);
-    ast.body['id'].index[0].type.should.equal('CallExpression');
-    ast.body['id'].index[0].callee.type.should.equal('Identifier');
-    ast.body['id'].index[0].callee.name.should.equal('plural');
-    ast.body['id'].index[0].arguments.length.should.equal(1);
-    ast.body['id'].index[0].arguments[0].type.should.equal('Identifier');
-    ast.body['id'].index[0].arguments[0].name.should.equal('m');
+    var ast = parse("id = {[ plural(m) ]} \nid[one] = foo");
+    ast['id']['_'].should.be.an.instanceOf(Object);
+    ast['id']['_']['one'].should.equal('foo');
+    ast['id']['_index'].length.should.equal(2);
+    ast['id']['_index'][0].should.equal('plural');
+    ast['id']['_index'][1].should.equal('m');
   });
-  it('plural macro errors', function() {
+  xit('plural macro errors', function() {
     var strings = [
       'id = {[ plural(m) ] \nid[one] = foo',
       'id = {[ plural(m) \nid[one] = foo',
@@ -64,13 +55,13 @@ describe('Example', function() {
     });
 
     for (var i in strings) {
-      var ast = parser.parse(strings[i]);
+      var ast = parse(strings[i]);
     } 
     errorsThrown.should.equal(strings.length);
   });
   it('comment', function() {
-    var ast = parser.parse('#test');
-    Object.keys(ast.body).length.should.equal(0);
+    var ast = parse('#test');
+    Object.keys(ast).length.should.equal(0);
   });
   it('comment errors', function() {
     var strings = [
@@ -79,46 +70,8 @@ describe('Example', function() {
       'f# foo',
     ];
     for (var i in strings) {
-      var ast = parser.parse(strings[i]);
-      Object.keys(ast.body).length.should.equal(0);
+      var ast = parse(strings[i]);
+      Object.keys(ast).length.should.equal(0);
     }  
-  });
-  it('complex string', function() {
-    var ast = parser.parse('test = test {{ var }} test2');
-    ast.body['test'].value.content[0].content.should.equal('test ');
-    ast.body['test'].value.content[1].name.should.equal('var');
-    ast.body['test'].value.content[2].content.should.equal(' test2');
-
-    var ast = parser.parse("test = test \\\" {{ var }} test2");
-    ast.body['test'].value.content[0].content.should.equal('test " ');
-    ast.body['test'].value.content[1].name.should.equal('var');
-    ast.body['test'].value.content[2].content.should.equal(' test2');
-  });
-  it('complex string errors', function() {
-    var strings = [
-      ['test = test {{ var ', 'Expected "}}"'],
-      ['test = test {{ var } ', 'Expected "}}"'],
-      ['test = test {{ var } }', 'Expected "}}"'],
-      ['test = test {{ var }} {{', 'Expected "}}"'],
-      ['test = test {{ var }} {{}', 'Expected "}}"'],
-      ['test = test {{ {{ }}', 'Expected "}}"'],
-      ['test = test {{ {{ } }}', 'Expected "}}"'],
-      ['test = test {{ {{ } }}}', 'Expected "}}"'],
-      ['test = test {{{ }', 'Expected "}}"'],
-    ];
-
-    var errorsThrown = 0;
-    parser.addEventListener('error', function() {
-      errorsThrown += 1;
-    });
-
-    for (var i in strings) {
-      var ast = parser.parse(strings[i][0]);
-    }
-    errorsThrown.should.equal(strings.length);
-  });
-  it('aria attribute', function() {
-    var ast = parser.parse('test = test\ntest.ariaLabel = aria foo');
-    ast.body['test'].attrs['aria-label'].value.content.should.equal('aria foo');
   });
 });
